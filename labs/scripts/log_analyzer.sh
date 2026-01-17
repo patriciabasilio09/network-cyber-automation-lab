c947ed61f221afa7cb690b8891a390aff2c6441f
Patricia Basilio <patriciabasilio882@gmail.com>
2026-01-17 10:19:30 -0600

    Phase 2 Day 4: Update log analysis automation script

--- labs/scripts/log_analyzer.sh
+++ labs/scripts/log_analyzer.sh
@@ -2,6 +2,19 @@
 
 LOGFILE="/var/log/auth.log"
 
-echo "Top attacking IPs (failed SSH logins):"
+THRESHOLD=3
+
+echo "Analyzing SSH failed loging attempts"
 echo "------------------------------------"
-sudo grep "sshd.*Failed password" $LOGFILE | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}' | sort | uniq -c | sort -nr | head -10
+
+grep "sshd.*Failed password" $LOGFILE | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}' | sort | uniq -c | sort -nr | head -10 | while read count ip; do
+if [ "$count" -ge "$THRESHOLD" ]; then
+ echo "[ALERT] Suspicious activity detected!"
+ echo "IP: $ip"
+ echo "Failed attempts: $count"
+ echo
+else
+ echo "[INFO] Normal activity: $ip ($count attempts)"
+fi
+done
+
