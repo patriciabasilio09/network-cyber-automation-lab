69526a00cacbd40db65a0d2cc36bd924c3059b6b
Patricia Basilio <patriciabasilio882@gmail.com>
2026-01-20 13:59:44 -0600

    Phase 2 Day 5 and 6: Notes for day 5 and 6. Added severity levels and timestamps to log analyzer

--- labs/scripts/log_analyzer.sh
+++ labs/scripts/log_analyzer.sh
@@ -2,19 +2,22 @@
 
 LOGFILE="/var/log/auth.log"
 
-THRESHOLD=3
-
 echo "Analyzing SSH failed loging attempts"
 echo "------------------------------------"
 
-grep "sshd.*Failed password" $LOGFILE | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}' | sort | uniq -c | sort -nr | head -10 | while read count ip; do
-if [ "$count" -ge "$THRESHOLD" ]; then
- echo "[ALERT] Suspicious activity detected!"
- echo "IP: $ip"
- echo "Failed attempts: $count"
- echo
+grep -a "sshd.*Failed password" $LOGFILE | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}' | sort | uniq -c | sort -nr | head -10 | while read count ip; do
+
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+
+if [ "$count" -ge 20 ]; then
+ SEVERITY="HIGH"
+elif [ "$count" -ge 10 ]; then
+ SEVERITY="MEDIUM"
+elif [ "$count" -ge 5 ]; then
+ SEVERITY="LOW"
 else
- echo "[INFO] Normal activity: $ip ($count attempts)"
+ SEVERITY="INFO"
 fi
+ echo "[$TIMESTAMP] [$SEVERITY]  IP: $ip | Failed Attempts: $count"
 done
 
